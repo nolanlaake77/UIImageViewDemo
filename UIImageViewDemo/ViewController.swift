4f4e4ba74720de2ef4d1d45392e9cb6190cf4879
Nolan Laake <iamchunkerdunkers@gmail.com>
2023-01-11 11:08:06 -0700

    MVP

--- UIImageViewDemo/ViewController.swift
+++ UIImageViewDemo/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    let myImage = UIImage(named: "me")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onButtonTap(_ sender: UIButton) {
+        let myImage = UIImage(named: "sss")
+        myImageView.image = myImage
+            
+    }
+    
 }
 
